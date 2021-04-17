e5c4eea43782c90822af4cb9cd7a6c34fb665c5f
StarHE99 <starhe@ucsc.edu>
2021-04-17 13:41:49 -0700

    renaming and git testing

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,8 +5,8 @@ class Play extends Phaser.Scene {
 
     preload() {
         this.load.image('starfield', 'assets/starfield.png');
-        this.load.image('rocket', 'assets/bob.png');
-        this.load.image('spaceship', 'assets/spaceship.png');
+        this.load.image('rocket', 'assets/devil.png');
+        this.load.image('spaceship', 'assets/angel.png');
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
