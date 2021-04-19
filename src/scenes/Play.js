0b81fc98393f0edf98ce4397bcc98b840c9b72e8
StarHE99 <starhe@ucsc.edu>
2021-04-19 15:32:28 -0700

    import new sound and explosion sheet/ missing 4 explosions

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -58,10 +58,10 @@ class Play extends Phaser.Scene {
             borderUISize + borderPadding,
             game.config.width,
             borderUISize * 2,
-            0x00FF00,
+            0xFFFFFF,
             ).setOrigin(0,0);
 
-        // white borders
+        // white borders change this
 	    this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
